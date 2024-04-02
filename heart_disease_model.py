3922f230a0f61b5d3b6f04c1ba0f838bc8230650
wiktoria-dega <wikidg4@gmail.com>
2024-04-02 22:45:44 +0200

    Add GridSearchCV for random forest

--- heart_disease_model.py
+++ heart_disease_model.py
@@ -16,6 +16,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.ensemble import RandomForestClassifier
 
 
 
@@ -83,6 +84,11 @@ print('accuracy')
 accuracy_td = accuracy_score(y_test, tree_preds_test)
 print(accuracy_td)
 
+for i,j in zip(tree.feature_importances_, X_train.columns):
+    print(i, j)
+    
+#ST_Slope_Flat i ChestPainType_TA i ChestPainType_NAP najmniej wazne
+
 #GridSearchCV TO DO
 params = {
     'max_depth': [None, 3, 5, 10],
@@ -103,25 +109,12 @@ gridSearch = GridSearchCV(tree, params, scoring=scoring, verbose=2, cv=4, refit=
 gridSearch.fit(X_train_norm, y_train)
 
 #sprawdzenie - predykcja najlepszego modelu
-pred_best_train = gridSearch.best_estimator_.predict(X_train_norm)
+#pred_best_train = gridSearch.best_estimator_.predict(X_train_norm)
 pred_best_test = gridSearch.best_estimator_.predict(X_test_norm)
 
-print('train - GridSearchCV')
-conf_test_gs_train = confusion_matrix(y_train, pred_best_train)
-print(conf_test_gs_train)
-
-print('classification report')
-print(classification_report(y_train, pred_best_train))
-
-print('accuracy')
-accuracy_gs_train = accuracy_score(y_train, pred_best_train)
-print(accuracy_gs_train)
-
-
-
 print('test - GridSearchCV')
-conf_test_gs_test = confusion_matrix(y_test, pred_best_test)
-print(conf_test_gs_test)
+conf_test_gs = confusion_matrix(y_test, pred_best_test)
+print(conf_test_gs)
 
 print('classification report')
 print(classification_report(y_test, pred_best_test))
@@ -130,4 +123,55 @@ print('accuracy')
 accuracy_gs_test = accuracy_score(y_test, pred_best_test)
 print(accuracy_gs_test)
 
+#las losowy
+rnd_forest = RandomForestClassifier()
+
+#trening
+rnd_forest.fit(X_train_norm, y_train)
+
+pred_forest = rnd_forest.predict(X_test_norm)
+
+print('test - Random Forest')
+conf_test_forest = confusion_matrix(y_test, pred_forest)
+print(conf_test_forest)
+
+print('classification report')
+print(classification_report(y_test, pred_forest))
+
+print('accuracy')
+accuracy_test_forest = accuracy_score(y_test, pred_forest)
+print(accuracy_test_forest)
+
+params_forest = {
+    'n_estimators': [100, 200, 300],
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [4, 6, 8, 10],
+    'max_features': ['auto', 'sqrt', 'log2']
+    }
+
+scoring_forest = {
+    'accuracy': make_scorer(accuracy_score),
+    'precision': make_scorer(precision_score, average='macro'),
+    'recall': make_scorer(recall_score, average='macro'),
+    'f1': make_scorer(f1_score, average='macro')
+    }
+
+gs_forest = GridSearchCV(rnd_forest, params_forest, scoring=scoring_forest, verbose=2, cv=4, refit='accuracy')
+
+#trening
+gs_forest.fit(X_train_norm, y_train)
+
+#sprawdzenie
+pred_best_forest_test = gs_forest.best_estimator_.predict(X_test_norm)
+
+print('test - GridSearchCV - Forest')
+conf_test_forest_gs = confusion_matrix(y_test, pred_best_forest_test)
+print(conf_test_forest_gs)
+
+print('classification report')
+print(classification_report(y_test, pred_best_forest_test))
+
+print('accuracy')
+accuracy_test_forest_gs = accuracy_score(y_test, pred_best_forest_test)
+print(accuracy_test_forest_gs)
 
